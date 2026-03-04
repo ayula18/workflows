1e7b00bd34fb6e2e34f6cfaeba15a1c9b2609401
ayula18 <69152956+ayula18@users.noreply.github.com>
2026-03-04 16:36:22 +0530

    mesa card 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import MesaExperience from "@/components/MesaExperience";
 import AutomationLibrary from "@/components/AutomationLibrary";
 import CallToAction from "@/components/CallToAction";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
+      <MesaExperience />
       <AutomationLibrary />
       <CallToAction />
     </main>
